b75a3cb0801daf97fd76929bcab09200f2dde6c4
ahmed abdel latief <ahmed_abd_ellatief@yahoo.com>
2025-04-26 21:39:37 +0300

    SMS

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -5,15 +5,13 @@ const config: CapacitorConfig = {
   appId: 'app.xpensia.com',
   appName: 'Xpensia',
   webDir: 'dist',
-  // Commenting out server URL for production build
-  // server: {
-  //   url: 'https://44f11ecc-0e77-4a7d-a302-6102d5d50c2b.lovableproject.com?forceHideBadge=true',
-  //   cleartext: true
-  // },
   plugins: {
     SplashScreen: {
       launchShowDuration: 0
-    }
+    },
+	SmsReaderPlugin: {
+    android: true
+  }
   },
   android: {
     appendUserAgent: 'Xpensia Android App'
