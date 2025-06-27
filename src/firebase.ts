daf10dbb2afa7acba1df3eaeb1e81d4d0cd81271
ahmed abdel latief <ahmed_abd_ellatief@yahoo.com>
2025-06-27 16:18:20 +0300

    firebase tokens

--- src/firebase.ts
+++ src/firebase.ts
@@ -2,10 +2,14 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: 'YOUR_API_KEY',
-  authDomain: 'YOUR_AUTH_DOMAIN',
-  projectId: 'YOUR_PROJECT_ID',
+  apiKey: 'AIzaSyAl0ikuXTAw_3u3GHVLRXcLANL9s3MBcxk',
+  authDomain: 'xpensia-505ac.firebaseapp.com',
+  projectId: 'xpensia-505ac',
   // Add other config fields as needed
+  storageBucket: "xpensia-505ac.firebasestorage.app",
+  messagingSenderId: "495648821355",
+  appId: "1:495648821355:web:fe5e00798f7fd290cb212e",
+  measurementId: "G-0LG7960PW8"
 };
 
 // Initialize Firebase app only once
