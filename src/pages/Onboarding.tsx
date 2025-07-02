9bf1e6b6d6f160a7f6cdb49d518f8e7bf0a4b110
aalatief1982 <ahmed.alatief@gmail.com>
2025-07-03 00:06:18 +0300

    Revert "Override back action during onboarding"

--- src/pages/Onboarding.tsx
+++ src/pages/Onboarding.tsx
@@ -1,24 +1,10 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import Layout from '@/components/Layout';
 import OnboardingSlides from '@/onboarding/OnboardingSlides';
 
 const Onboarding = () => {
   const navigate = useNavigate();
-
-  useEffect(() => {
-    const handlePopState = (event: PopStateEvent) => {
-      event.preventDefault();
-      window.history.pushState(null, '', window.location.href);
-    };
-
-    window.history.pushState(null, '', window.location.href);
-    window.addEventListener('popstate', handlePopState);
-
-    return () => {
-      window.removeEventListener('popstate', handlePopState);
-    };
-  }, []);
 
   const handleComplete = () => {
     localStorage.setItem('xpensia_onb_done', 'true');
